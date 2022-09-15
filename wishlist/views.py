f5d6ac6f5586c6159d604421aa83504b30fa71af
yoozu <abiyyuv77@gmail.com>
2022-09-15 16:08:00 +0700

    Lab 2

--- wishlist/views.py
+++ wishlist/views.py
@@ -22,7 +22,7 @@ def show_json(request):
 def show_json_by_id(request,id):
     data = BarangWishlist.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
-def show_xml_by_id(request):
+def show_xml_by_id(request,id):
     data = BarangWishlist.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
