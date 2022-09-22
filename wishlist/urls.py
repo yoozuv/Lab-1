837041f75e5405568f212587035d77bd7f4308f5
yoozu <abiyyuv77@gmail.com>
2022-09-22 13:27:22 +0700

    Lab 3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from wishlist.views import show_wishlist,show_xml,show_json,show_json_by_id,show_xml_by_id
+from wishlist.views import show_wishlist,show_xml,show_json,show_json_by_id,show_xml_by_id,register,login_user,logout_user
+
 
 
 
@@ -11,4 +12,8 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),  
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'), #sesuaikan dengan nama fungsi yang dibuat
     path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
+    
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
